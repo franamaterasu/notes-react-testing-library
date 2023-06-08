d05c076aad652d7744e86d5b639253c6f6236cd3
Fran <franvlive@gmail.com>
2023-06-08 09:49:21 +0200

    6. Grouping jest tests

--- src/components/Jest/2.Greet-tdd/greetTDD.test.tsx
+++ src/components/Jest/2.Greet-tdd/greetTDD.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { GreetTDD } from "./";
+import { GreetTDD } from ".";
 
 test("Greet render correctly with TDD", () => {
   render(<GreetTDD />);
