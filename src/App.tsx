bc23bda64dbe530cd5e637f361006ec9d93010f7
Fran <franvlive@gmail.com>
2023-06-09 08:27:52 +0200

    18 - React testing library - Query individual elements and multiple elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Form } from "./components/RTL/form";
+import { Skills } from "./components/RTL-query-multiple-elements/skills";
+import { Form } from "./components/RTL-query-individual-element/form";
+
+const skillsList = ["HTML", "CSS", "Javascript", "React", "Jest", "RTL"];
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <hr />
+      <Skills skills={skillsList} />
     </div>
   );
 }
